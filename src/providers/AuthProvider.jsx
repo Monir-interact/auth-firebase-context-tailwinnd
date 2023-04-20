22fc5c5c35ef6ce6431a2d9ce85f92f2fbbc5312
A Z M Monir Hossain <monir@AZMs-MacBook-Pro.local>
2023-04-20 10:33:21 +0600

    set loading

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -14,6 +14,7 @@ const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   const createUser = (email, password) => {
     return createUserWithEmailAndPassword(auth, email, password);
@@ -31,6 +32,7 @@ const AuthProvider = ({ children }) => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       console.log("auth state changed", currentUser);
       setUser(currentUser);
+      setLoading(false);
     });
     return () => {
       unsubscribe();
@@ -39,6 +41,7 @@ const AuthProvider = ({ children }) => {
 
   const authInfo = {
     user,
+    loading,
     createUser,
     signIn,
     logOut,
